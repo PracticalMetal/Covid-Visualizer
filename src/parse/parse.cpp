e33bea0bd45121ec9eb0dce85aff9c6c1f35a685
PracticalMetal <2pulkit2@gmail.com>
2021-07-10 18:41:37 +0530

    Implement libcurl

--- src/parse/parse.cpp
+++ src/parse/parse.cpp
@@ -4,7 +4,29 @@
 #include <iostream>  
 #include <curl/curl.h> 
 using namespace std;
- 
+
+size_t got_data(char *buffer, size_t itemsize, size_t nitems, void *ignorethis)
+{
+    size_t bytes=itemsize*nitems;
+    int linenumber=1;
+
+
+    cout<<"The size of the chunck is "<<bytes<<" bytes\n";
+    //printf("%d:\t",linenumber);
+    cout<<linenumber<<":\t";
+    for(long unsigned int i=0;i<bytes;i++)
+    {
+        cout<<buffer[i];
+        if(buffer[i]=='\n')
+        {
+            linenumber++;
+            cout<<linenumber<<":\t";
+        }
+    }
+    cout<<"\n\n";
+    return bytes;
+}
+
 int main()
 {
     CURL *curl=curl_easy_init();
@@ -16,7 +38,9 @@ int main()
     }
 
     // set options
-    curl_easy_setopt(curl, CURLOPT_URL, "https://jacobsorber.com");
+    //curl_easy_setopt(curl, CURLOPT_URL, "https://jacobsorber.com");
+    curl_easy_setopt(curl, CURLOPT_URL, "https://www.mygov.in/covid-19");
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, got_data);
 
     // perform actions
     CURLcode result=curl_easy_perform(curl);
